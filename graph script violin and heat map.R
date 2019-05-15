ac7295a1d6162a32adc13f25e384560934e43199
jorismoorkamp <49749873+jorismoorkamp@users.noreply.github.com>
2019-05-15 11:45:53 +0200

    Add files via upload

--- graph script violin and heat map.R
+++ graph script violin and heat map.R
@@ -1,9 +1,5 @@
 library(ggplot2)
 
-#creates a subset of the full data with only the data for time=10 and then a subset of this subset for a cladogenesis rate of 1
-DAISIETable2 <- subset(DAISIETable, time==4)
-DAISIETable3 <- subset(DAISIETable2, lambda_c_sim==2.5)
-
 
 #creates a heat map with the proportion endemic plotted against the proportion of the mainland area split into facets along time and cladogenesis rate
 ggplot(DAISIETable, aes(x=prop_endemic,y=prop_mainland,fill=log(lambda_c))) + geom_tile() +facet_grid(lambda_c_sim ~ time)
@@ -16,4 +12,14 @@ D2 <- subset(D1, lambda_c_sim==2.5)
 #plots the maximum likelihood cladogenesis rate distribution in a grid of proportion of the mainland and proportion nonendemic
 ggplot(D2, aes(x=0, y= lambda_c)) + geom_violin() + geom_dotplot(binaxis = "y", binwidth = .5, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland) + ylim(0,15)
 
-ggplot(D2, aes(x=0, y= gamma)) + geom_violin() + geom_dotplot(binaxis = "y", binwidth = .001, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland)
+ggplot(D2, aes(x=0, y= gamma)) + geom_violin() + geom_dotplot(binaxis = "y", binwidth = .001, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland) 
+
+ggplot(D2, aes(x=0, y= mu)) + geom_violin() + geom_dotplot(binaxis = "y", binwidth = .2, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland)
+
+ggplot(D2, aes(x=0, y= lambda_a)) + geom_violin() + geom_dotplot(binaxis = "y", binwidth = .5, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland) + scale_y_log10()
+
+ggplot(D2, aes(x=0, y= K)) + geom_violin() + scale_y_log10() + geom_dotplot(binaxis = "y", binwidth = .05, stackdir="center")
+
+D3 <- subset(D2, K < 100)
+
+ggplot(D3, aes(x=0, y= K)) + geom_violin() + scale_y_log10() + geom_dotplot(binaxis = "y", binwidth = .05, stackdir="center") + facet_grid(prop_endemic ~ prop_mainland)
