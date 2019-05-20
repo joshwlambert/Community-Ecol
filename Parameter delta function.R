a8711e6cdd63925fc2b24ca912352043df40e48f
joshwlambert <j.w.l.lambert@rug.nl>
2019-05-20 14:58:06 +0200

    added if statement to calculate delta for immigration rate

--- Parameter delta function.R
+++ Parameter delta function.R
@@ -102,12 +102,60 @@ model_differences <- function(data, island_age, clado_rate, ext_rate, immig_rate
     nonoceanic_mean_mu <- mean(nonoceanic_abs_diff$mu)
     
     #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
-    diff <- (oceanic_mean_mu - nonoceanic_mean_mu)
+    delta <- (oceanic_mean_mu - nonoceanic_mean_mu)
     
     #Calculate the relative error of the differences
-    relative_delta <- diff/mean(DAISIEdata$mu_sim)
+    relative_delta <- delta/mean(DAISIEdata$mu_sim)
     
   }
+  
+  if (immig_diff == TRUE) {
+    #isolate for a given value of cladogenesis
+    oceanic_time_lac <- dplyr::filter(oceanic_time, lambda_c_sim == clado_rate)  
+    
+    #isolate for a given value of extinction
+    oceanic_time_lac_mu <- dplyr::filter(oceanic_time_lac, mu_sim == ext_rate)
+    
+    #calculate absolute difference between ML estimate and true value
+    oceanic_abs_diff <- dplyr::mutate(oceanic_time_lac_mu, abs(gamma - gamma_sim))
+    
+    #remove any NA values
+    oceanic_abs_diff <- tidyr::drop_na(oceanic_abs_diff)
+    
+    #deal with any outliers
+    #anomalize::anomalize(oceanic_abs_diff, oceanic_abs_diff$lambda_c)
+    
+    #calculate the average of the absolute differences
+    oceanic_mean_gamma <- mean(oceanic_abs_diff$gamma)
+    
+    #isolate for a given value of cladogenesis
+    nonoceanic_time_lac <- dplyr::filter(nonoceanic_time, lambda_c_sim == clado_rate)
+    
+    #isolate for a given value of extinction
+    nonoceanic_time_lac_mu <- dplyr::filter(nonoceanic_time_lac, mu_sim == ext_rate)
+    
+    #isolate for a given proportion of mainland 
+    nonoceanic_time_lac_mu_mainland <- dplyr::filter(nonoceanic_time_lac_mu, prop_mainland == mainland)
+    
+    #isolate for a given proportion of nonendemics 
+    nonoceanic_time_lac_mu_mainland_nonend <- dplyr::filter(nonoceanic_time_lac_mu_mainland, prop_non_endemic == nonendemic)
+    
+    #calculate the absolute difference between ML estimate and true value
+    nonoceanic_abs_diff <- dplyr::mutate(nonoceanic_time_lac_mu_mainland_nonend, abs(gamma - gamma_sim))
+    
+    #remove any NA values
+    nonoceanic_abs_diff <- tidyr::drop_na(nonoceanic_abs_diff)
+    
+    #calculate the average of the absolute differences
+    nonoceanic_mean_gamma <- mean(nonoceanic_abs_diff$gamma)
+    
+    #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
+    delta <- (oceanic_mean_gamma - nonoceanic_mean_gamma)
+    
+    #Calculate the relative error of the differences
+    relative_delta <- delta/mean(DAISIEdata$gamma_sim)
+  }
+  
   return(relative_delta)
 }
 
