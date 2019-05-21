62dbbbc9d338d4b791027134f444c9f99943c4fc
joshwlambert <j.w.l.lambert@rug.nl>
2019-05-21 11:23:38 +0200

    finished function

--- R/model_param_delta.R
+++ R/model_param_delta.R
@@ -26,39 +26,29 @@ model_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate
   nonoceanic_time <- dplyr::filter(nonoceanic, time == island_age)
   
   if (clado_diff == TRUE) {
-    relative_delta <- clado_param_delta(data, island_age, clado_rate, ext_rate, immig_rate, 
-                                        ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                                        ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                                        oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time)
+    relative_delta <- clado_param_delta(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                                        clado_rate, ext_rate, mainland, nonendemic)
   }
   
   if (ext_diff == TRUE) {
-    relative_delta <- ext_param_delta(data, island_age, clado_rate, ext_rate, immig_rate, 
-                                      ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                                      ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                                      oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time)
+    relative_delta <- ext_param_delta(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                                      clado_rate, ext_rate, mainland, nonendemic)
   }
   
   if (immig_diff == TRUE) {
-    relative_delta <- immig_param_delta(data, island_age, clado_rate, ext_rate, immig_rate, 
-                                        ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                                        ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                                        oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time)
+    relative_delta <- immig_param_delta(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,data, 
+                                        clado_rate, ext_rate, immig_rate, mainland, nonendemic)
   }
   
   if (ana_diff == TRUE) {
-    relative_delta <- ana_param_delta(data, island_age, clado_rate, ext_rate, immig_rate, 
-                                      ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                                      ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                                      oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time)
+    relative_delta <- ana_param_delta(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                                      clado_rate, ext_rate, ana_rate, mainland, nonendemic)
   }
   return(relative_delta)
 }
 
-clado_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate, 
-                              mainland, nonendemic, clado_diff = TRUE, 
-                              ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                              oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time) {
+clado_param_delta <- function(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                              clado_rate, ext_rate, mainland, nonendemic) {
   
   #isolate for a given value of cladogenesis
 
@@ -80,17 +70,15 @@ clado_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate
   #deal with any outliers
   nonoceanic_mean_lac <- mean(nonoceanic_abs_diff$nonoceanic_delta_lac) #calculate the average of the absolute differences
   
-  delta <- (oceanic_mean_lac - nonoceanic_mean_lac) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
+  delta <- abs(oceanic_mean_lac - nonoceanic_mean_lac) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
   
   relative_delta <- delta/clado_rate #Calculate the relative error of the differences
   
   return(relative_delta)
 }
 
-ext_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate, 
-                            ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                            ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                            oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time) {
+ext_param_delta <- function(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                            clado_rate, ext_rate, mainland, nonendemic) {
   
   oceanic_abs_diff <- oceanic_time %>% 
     dplyr::filter(lambda_c_sim == clado_rate) %>% #isolate for a given value of cladogenesis
@@ -110,17 +98,15 @@ ext_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate,
   #deal with any outliers
   nonoceanic_mean_mu <- mean(nonoceanic_abs_diff$nonoceanic_delta_mu) #calculate the average of the absolute differences
 
-  delta <- (oceanic_mean_mu - nonoceanic_mean_mu) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
+  delta <- abs(oceanic_mean_mu - nonoceanic_mean_mu) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
   
   relative_delta <- delta/ext_rate #Calculate the relative error of the differences
   
   return(relative_delta)
 }
 
-immig_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate, 
-                              ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                              ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                              oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time) {
+immig_param_delta <- function(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,data, 
+                              clado_rate, ext_rate, immig_rate, mainland, nonendemic) {
   
   oceanic_abs_diff <- oceanic_time %>%
     dplyr::filter(lambda_c_sim == clado_rate) %>% #isolate for a given value of cladogenesis
@@ -140,17 +126,15 @@ immig_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate
   #deal with any outliers
   nonoceanic_mean_gamma <- mean(nonoceanic_abs_diff$nonoceanic_delta_gam) #calculate the average of the absolute differences
 
-  delta <- (oceanic_mean_gamma - nonoceanic_mean_gamma) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
+  delta <- abs(oceanic_mean_gamma - nonoceanic_mean_gamma) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
 
   relative_delta <- delta/immig_rate #Calculate the relative error of the differences
   
   return(relative_delta)
 }
 
-ana_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate, 
-                            ana_rate, mainland, nonendemic, clado_diff = TRUE, 
-                            ext_diff = FALSE, immig_diff = FALSE, ana_diff = FALSE,
-                            oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time) {
+ana_param_delta <- function(oceanic_time = oceanic_time, nonoceanic_time = nonoceanic_time,
+                            clado_rate, ext_rate, ana_rate, mainland, nonendemic) {
   
   oceanic_abs_diff <- oceanic_time %>%
     dplyr::filter(lambda_c_sim == clado_rate) %>% #isolate for a given value of cladogenesis
@@ -170,7 +154,9 @@ ana_param_delta <- function(data, island_age, clado_rate, ext_rate, immig_rate,
   #deal with any outliers
   nonoceanic_mean_laa <- mean(nonoceanic_abs_diff$nonoceanic_delta_laa) #calculate the average of the absolute differences
   
-  delta <- (oceanic_mean_laa - nonoceanic_mean_laa) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
+  delta <- abs(oceanic_mean_laa - nonoceanic_mean_laa) #Calculate the difference betweeen the oceanic differences and the nonoceanic difference
   
   relative_delta <- delta/ana_rate #Calculate the relative error of the differences
+  
+  return(relative_delta)
 }
