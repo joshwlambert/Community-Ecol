d9e646a410d1d49282798d2238ba8782bff67877
joshwlambert <j.w.l.lambert@rug.nl>
2019-05-23 22:33:32 +0200

    statisticalanalysis

--- R/stat_testing.R
+++ R/stat_testing.R
@@ -1,3 +1,7 @@
+library(multcomp)
+library(dplyr)
+library(ggplot2)
+
 #Isolate the requried data
 oceanic_four <- final_data %>%
   dplyr::filter(island_type == 'oceanic') %>%
@@ -83,9 +87,11 @@ shapiro.test(log(nonoceanic_four_one_nine$mu))
 shapiro.test(log(nonoceanic_four_one_nine$gamma))
 shapiro.test(log(nonoceanic_four_one_nine$lambda_a))
 
+##########################
+FINISH
+##########################
 #Barlett test for homogeneity of variances
-bartlett.test(log(oceanic_four$lambda_c), log(nonoceanic_four_one_nine$lambda_c), data = temp_data)
-bartlett.test(count ~ spray, data=InsectSprays)
+bartlett.test(log(oceanic_four$lambda_c), log(nonoceanic_four_one_nine$lambda_c), data = )
 
 #Paired t-test
 test1 <- t.test(oceanic_four$lambda_c, nonoceanic_four_one_nine$lambda_c, paired=TRUE, alt="less")
@@ -96,17 +102,18 @@ tests <- #data.frame of p values
 p.adjust(method = 'bonferroni')
 
 
-#One-way ANOVA 
-anova <- aov(oceanic_four$lambda_c, nonoceanic_four_one_nine$lambda_c)
+#Transform the data
+oceanic_nonoceanic <- final_data %>%
+  dplyr::filter(time == 4) 
+attach(oceanic_nonoceanic)
+#dependent variable island type
+#independent variable parameter estimate
 
+#One-way ANOVA
+anova1 <- aov(lambda_c ~ island)
+#post-hoc p-value adjustment
+TukeyHSD(anova1)
 
-fit1 = lm(formula = log(nonoceanic_four_one_nine[, 1]) ~ log(oceanic_four$lambda_c[, 1]))
-fit2 = 
-fit3 
-anova(c(fits))
-TukeyHSD(fit)
-
-pairwise.t.test(x = oceanic_four$lambda_c, g = nonoceanic_four_one_nine$lambda_c, p.adjust.method = 'none')
-
-
+#Pairwise t-test
+pairwise.t.test(x= lambda_c, g = island, data = oceanic_nonoceanic,  p.adj = "bonferroni")
 
